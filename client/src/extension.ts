67f2363ebc72bc9a1474b2623ace378c06283e39
superveetz <alexdiv87@hotmail.com>
2018-06-05 22:24:37 -0700

    syntax coloring implemented

--- client/src/extension.ts
+++ client/src/extension.ts
@@ -12,7 +12,7 @@ import { LanguageClient, LanguageClientOptions, ServerOptions, TransportKind } f
 export function activate(context: ExtensionContext) {
 
 	// The server is implemented in node
-	let serverModule = context.asAbsolutePath(path.join('server', 'server.js'));
+	let serverModule = context.asAbsolutePath(path.join('../server/src', 'server.js'));
 	// The debug options for the server
 	let debugOptions = { execArgv: ["--nolazy", "--inspect=6009"] };
 	
@@ -29,14 +29,14 @@ export function activate(context: ExtensionContext) {
 		documentSelector: [{scheme: 'file', language: 'mox'}],
 		synchronize: {
 			// Synchronize the setting section 'languageServerExample' to the server
-			configurationSection: 'lspSample',
+			configurationSection: 'vscode-mox-extension',
 			// Notify the server about file changes to '.clientrc files contain in the workspace
 			fileEvents: workspace.createFileSystemWatcher('**/.clientrc')
 		}
 	}
 	
 	// Create the language client and start the client.
-	let disposable = new LanguageClient('lspSample', 'Language Server Example', serverOptions, clientOptions).start();
+	let disposable = new LanguageClient('vscode-mox-extension', 'Mox Language Server', serverOptions, clientOptions).start();
 	
 	// Push the disposable to the context's subscriptions so that the 
 	// client can be deactivated on extension deactivation
